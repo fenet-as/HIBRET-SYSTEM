08c4e6775a1ad099667daa351da13fca865fc220
Fenet <fenetasmamaw48@gmail.com>
2026-05-22 23:21:40 +0300

    added .env

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import util.DatabaseInitializer;
+
 /**
  * HIBRET-SYSTEM - Entry Point
  * Main class to start the application
@@ -7,5 +9,8 @@ public class Main {
   public static void main(String[] args) {
     // Application entry point
     System.out.println("HIBRET-SYSTEM Starting...");
+    DatabaseInitializer.init();
+
+    System.out.println("🚀 System Ready");
   }
 }
