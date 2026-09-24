9820ad61e13e4949acca9bc48717b122edd6b659
Fenet <fenetasmamaw48@gmail.com>
2026-09-24 10:43:27 +0300

    chore: remove env file from repository

--- src/util/AppConstants.java
+++
@@ -1,5 +0,0 @@
-package util;
-
-public class AppConstants {
-  // Application-wide constants
-}
--- src/util/PasswordHasher.java
+++
@@ -1,5 +0,0 @@
-package util;
-
-public class PasswordHasher {
-  // Password hashing and security utilities
-}
